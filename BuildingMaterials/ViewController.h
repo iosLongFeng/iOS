33f72caaaf22c7f459dae9a5c48663678f3f6ec1
龙丰 <longfeng@12345.local>
2016-03-24 11:11:53 +0800

    Initial Commit

---
+++ BuildingMaterials/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BuildingMaterials
+//
+//  Created by 龙丰 on 16/3/24.
+//  Copyright © 2016年 longfeng.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BuildingMaterials/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BuildingMaterials
+//
+//  Created by 龙丰 on 16/3/24.
+//  Copyright © 2016年 longfeng.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
